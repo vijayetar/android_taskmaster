87914d07b0b42c179c237f456db7b6b332d0c525
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-10-19 15:24:03 -0700

    the buttons are clickable

--- app/src/main/java/com/vijayetar/mytasks/MainActivity.java
+++ app/src/main/java/com/vijayetar/mytasks/MainActivity.java
@@ -3,6 +3,8 @@ package com.vijayetar.mytasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addTask = MainActivity.this.findViewById(R.id.addTask);
+        Button allTasks = MainActivity.this.findViewById(R.id.allTasks);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                System.out.println("we are adding a task");
+            }
+        });
+        allTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                System.out.println("we are reviewing all tasks");
+            }
+        });
     }
 }
