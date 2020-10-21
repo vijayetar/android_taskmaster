ae4e8d5a87b774b0a1baea97026c342a50e8985c
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-10-21 16:54:49 -0700

    connected username from settings page to home page

--- app/src/main/java/com/vijayetar/mytasks/MainActivity.java
+++ app/src/main/java/com/vijayetar/mytasks/MainActivity.java
@@ -3,17 +3,33 @@ package com.vijayetar.mytasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        // this is setting the username above the buttons in a textview or else get the preferences from the settings activity
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView declareUsername = findViewById(R.id.enterUserNameTextV);
+        declareUsername.setText(preferences.getString("userName", "Go to settings to enter username")); //this is default string if username is not available
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // this is button taking you to the add task and all tasks activity
         Button addTask = MainActivity.this.findViewById(R.id.addTask);
         Button allTasks = MainActivity.this.findViewById(R.id.allTasks);
         ImageButton settingsButton = MainActivity.this.findViewById(R.id.goToSettings);
