43b0ced8a3d99635481061e8628c0e8d47591737
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-10-23 13:05:44 -0700

    selected title rendering on the taskdetail page

--- app/src/main/java/com/vijayetar/mytasks/TaskDetail.java
+++ app/src/main/java/com/vijayetar/mytasks/TaskDetail.java
@@ -2,7 +2,9 @@ package com.vijayetar.mytasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaskDetail extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class TaskDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+        Intent intent = getIntent();
+        String titleName = intent.getStringExtra("title");
+        String bodyName= intent.getStringExtra("body");
+        String stateName = intent.getStringExtra("state");
+        TextView title = findViewById(R.id.taskDetailNameTV);
+        title.setText(titleName);
     }
 }
